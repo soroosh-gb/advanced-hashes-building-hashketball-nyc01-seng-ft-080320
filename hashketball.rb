189ed0c586929c628825e86037c41616b5d343b7
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-07-09 01:32:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,8 +7,7 @@ def game_hash()
     :team_name => "Brooklyn Nets" ,
     :colors => ["Black","White"] ,
     :players => [
-      { 
-        :player_name => "Alan Anderson",
+       "Alan Anderson" =>
         :number => 0,
         :shoe => 16,
         :points => 22,
